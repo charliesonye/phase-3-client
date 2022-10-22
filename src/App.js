fe8a5dbe39411b7c3704e60f6bf9cebbe3d08ffa
Charles <c_onyemem@live.com>
2022-10-22 13:37:26 -0700

    Defined routes to components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
 
 function App() {
   return (
     <div className="App">
+        <Router>
+        <NavBar />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/technicians' element={<TechnicianList />}  />
+            <Route path='/technicians/:id' element={<Technician />} />
+            <Route path='/customer_form' element={<CustomerForm />} />
+            <Route path='/customer/:id' element={<Customer />} />
+          </Routes>
       </header>
+      </Router>
     </div>
   );
 }

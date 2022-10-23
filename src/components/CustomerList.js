053fe71691fca60f0a4e029723181dd029250b01
Charles <c_onyemem@live.com>
2022-10-23 15:20:20 -0700

    successfully receiving list of customers with fetch request to backend

--- src/components/CustomerList.js
+++ src/components/CustomerList.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function CustomerList() {
+    const [customers, setCustomers] = useState([])
+
+    useEffect(()=>{
+        fetch('http://localhost:9292/customers')
+        .then(res => res.json())
+        .then(data => setCustomers(data))
+    }, [])
+
+    
   return (
     <div>CustomerList</div>
   )
